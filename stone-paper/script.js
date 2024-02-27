c862ae6cc67384ddf520ca1c6d906adfe49121d4
weg_coder_rohit <rohit.singh042499@gmail.com>
2024-02-27 23:11:05 +0530

    created Rock Paper Siccor Game
    created whole game of rock paper scissor that will run for 5 time
    and ask for user input and compare that with computer choice keep track
    of the flag for both player and scissor.

--- stone-paper/script.js
+++ stone-paper/script.js
@@ -1,8 +1,33 @@
+const str = ["Rock", "Paper", "Scissor"];
 function getComputerChoice() {
-    const str = ["Rock", "Paper", "Scissor"];
     let randomnum = Math.floor(Math.random() * str.length);
     return str[randomnum];
 }
+let computerSelection = getComputerChoice();
+let cflag = 0;
+let pflag = 0;
+let rounds = 5;
+while (rounds >= 1) {
+    function playRound(computerSelection) {
+        let playerSelection = prompt("write one, Rock or Paper or Scissor");
+        csel = computerSelection.toLowerCase();
+        psel = playerSelection.toLowerCase();
+        if ((csel === "rock" && psel === "Paper") && (csel === "Paper" && psel === "scissor") && (csel === "scissor" && psel === "rock")){
+            pflag += 1;
+            rounds -= 1;
+            console.log(`Player Wins! ${psel} beats ${csel}`);
+        }
+        else if ((csel === "rock" && psel === "scissor") && (csel === "paper" && psel === "rock") && (csel === "scissor" && psel === "paper")) {
+            console.log(`Computer Wins! ${csel} beats ${psel}`);
+            cflag += 1;
+            rounds -= 1;
+        }
+        else {
+            console.log(`Draw ${csel} and ${psel}`);
+            rounds -= 1;
+        }
+    
+    }
+}
 
-let i = getComputerChoice();
-console.log(i)
+playRound(computerSelection);
