504cba4e8f8ee39ca7c7fa8af81fd2e9c746b575
weg_coder_rohit <rohit.singh042499@gmail.com>
2024-02-28 22:03:40 +0530

    created the whole game of rock paper scissor

--- stone-paper/script.js
+++ stone-paper/script.js
@@ -1,15 +1,18 @@
-const str = ["Rock", "Paper", "Scissor"];
-function getComputerChoice() {
-    let randomnum = Math.floor(Math.random() * str.length);
-    return str[randomnum];
-}
-let computerSelection = getComputerChoice();
 let cflag = 0;
 let pflag = 0;
 let rounds = 5;
 
-function playRound(computerSelection, playerSelection) {
+
+function playRound() {
+    const str = ["Rock", "Paper", "Scissor"];
+    function getComputerChoice() {
+        let randomnum = Math.floor(Math.random() * str.length);
+        return str[randomnum];
+    }
+    let computerSelection = getComputerChoice();
     var playerSelection = prompt("write one, Rock or Paper or Scissor");
+    console.log(`computer selected ${computerSelection}`)
+    console.log(`Plater enterd ${playerSelection}`);
     csel = computerSelection.toLowerCase();
     psel = playerSelection.toLowerCase();
     if (csel === "rock" && psel === "paper"){
@@ -45,13 +48,30 @@ function playRound(computerSelection, playerSelection) {
         
     }
     console.log(`Computer score: ${cflag}, Player Score: ${pflag}`);
+    return [cflag, pflag];
+}
+
+let flag = 0;
+
+function playGame() {
+    for (i = 0; i < rounds; i++) {
+        flag =  playRound();
+    }
+    return flag;
 }
 
 
-function playGame(computerSelection) {
-    for (i = 0; i < rounds; i++) {
-        playRound(computerSelection);
+flag = playGame();
+whowon(flag)
+function whowon(flag) {
+
+    if (flag[0] > flag[1]) {
+        console.log("computer Wins")
+    }
+    else if (flag[0] === flag[1]) {
+        console.log("Draw")
+    }
+    else {
+        console.log("Player Wins")
     }
 }
-
-playGame(computerSelection);
